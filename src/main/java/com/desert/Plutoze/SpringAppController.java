4f1f5f949b322e0dbd2ce195da2b6495dc28722f
Anianonymous <hegdeanirudh2003@gmail.com>
2023-12-31 11:42:37 +0530

    Initial commit

---
+++ src/main/java/com/desert/Plutoze/SpringAppController.java
@@ -0,0 +1,32 @@
+//package com.desert.Plutoze;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class FunController {
+//    @GetMapping("/")
+//    public String hello(){
+//        return "hello";
+//    }
+//}
+
+package com.desert.Plutoze;
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SpringAppController {
+    @GetMapping("/")
+    public String welcome() {
+        return "index";
+    }
+
+    @GetMapping("/viewprofile")
+    public String view() {
+        return "home";
+    }
+}
---
+++ src/test/java/com/desert/Plutoze/PlutozeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desert.Plutoze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutozeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
