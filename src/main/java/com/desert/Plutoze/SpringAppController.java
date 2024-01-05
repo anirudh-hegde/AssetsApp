379c6419755f404daf1a49ddd76e8b3892b53f08
Anianonymous <hegdeanirudh2003@gmail.com>
2024-01-05 10:45:56 +0530

    altered files

--- src/main/java/com/desert/Plutoze/SpringAppController.java
+++ src/main/java/com/desert/Plutoze/SpringAppController.java
@@ -2,12 +2,15 @@ package com.desert.Plutoze;
 
 //import org.springframework.stereotype.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SpringAppController {
+
+
     @GetMapping("/")
     public String welcome() {
         return "index";
